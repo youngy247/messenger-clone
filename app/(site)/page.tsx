116bbf85ffc8e01f9068441bcf140f33d466b054
Adam Young <adamyoungy678@gmail.com>
2023-07-11 16:32:23 +0100

    feat: added messenger logo to middle of page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+
 export default function Home() {
     return (
-      <div>
-      <p className="text-sky-500 text-3xl">Hello Index Page!</p>
+      <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+      "
+      >
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <Image 
+              alt="messenger logo"
+              height="48"
+              width="48"
+              className="mx-auto w-auto"
+              src="/images/logo.png"
+             />
+        </div>
       </div>
     )
   }
