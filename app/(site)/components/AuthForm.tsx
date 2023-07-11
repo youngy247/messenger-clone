a3236a906b7cce0d8cd556c36c3669e3dff23114
Adam Young <adamyoungy678@gmail.com>
2023-07-11 17:02:03 +0100

    feat: added styling to form container

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -50,7 +50,28 @@ const AuthForm = () => {
     }
 
     return ( 
-        <div>Auth Form!</div>
+        <div
+            className="
+                mt-8
+                sm:mx-auto
+                sm:w-full
+                sm:max-w-md
+            "
+        >
+            <div
+                className="
+                    bg-white
+                    px-4
+                    py-8
+                    shadow
+                    sm:rounded-lg
+                    sm:px-10
+                "
+            >
+
+            </div>
+
+        </div>
      );
 }
  
