5989b601009a4b29990a13ece0e0787a12a21480
Adam Young <adamyoungy678@gmail.com>
2023-07-11 16:50:21 +0100

    feat: created loading state and toggleVariant function

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,21 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
     const [variant, setVariant] = useState<Variant>('LOGIN')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER')
+        } else {
+            setVariant('LOGIN')
+        }
+    }, [variant])
+    
     return ( 
         <div>Auth Form!</div>
      );
