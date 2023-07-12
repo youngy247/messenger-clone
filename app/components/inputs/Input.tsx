45c3ec8cc881587f3193e082acaffcc3587b2b82
Adam Young <adamyoungy678@gmail.com>
2023-07-12 11:09:08 +0100

    feat: Refactor Input component and add PropTypes

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,4 +1,31 @@
-const Input = () => {
+'use client';
+
+import clsx from "clsx";
+import {
+    FieldErrors, 
+    FieldValues,
+    UseFormRegister
+} from 'react-hook-form';
+
+interface InputProps {
+    label: string
+    id: string
+    type?: string
+    required?: boolean
+    register: UseFormRegister<FieldValues>
+    errors: FieldErrors,
+    disabled?: boolean
+}
+
+const Input: React.FC<InputProps> = ({
+    label,
+    id,
+    type,
+    required,
+    register,
+    errors,
+    disabled
+}) => {
     return ( 
         <div>
             I am an Input!
