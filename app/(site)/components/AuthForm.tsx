4e571ff2e167d5dcc73881152ede9d31cfa230d5
Adam Young <adamyoungy678@gmail.com>
2023-07-12 13:38:10 +0100

    feat: created thematic break for contining login with social media

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -107,8 +107,42 @@ const AuthForm = () => {
                         </Button>
                     </div>
                 </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div
+                            className="
+                                absolute
+                                inset-0
+                                flex
+                                items-center
+                            "
+                        >
+                            <div 
+                                className="
+                                    w-full border-t
+                                    border-gray-300
+                                " 
+                            /> 
+                        </div>
+                        <div 
+                            className="
+                                    relative 
+                                    flex 
+                                    justify-center 
+                                    text-sm
+                                "
+                                >
+                            <span 
+                                className="
+                                    bg-white 
+                                    px-2 
+                                    text-gray-500">
+                                        Or continue with
+                            </span>
 
-
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
